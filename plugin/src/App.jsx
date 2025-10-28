773fc2c8c62a596511250dff696c90c0ead23409
Sameer Kumar <sameerkumar10122004@gmail.com>
2025-10-28 11:37:05 +0530

    test

--- plugin/src/App.jsx
+++ plugin/src/App.jsx
@@ -6,6 +6,8 @@ import toast, { Toaster } from "react-hot-toast";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 
+console.log("Plugin API URL:", process.env.NEXT_PUBLIC_API_URL);
+
 const BugReportSchema = Yup.object().shape({
   title: Yup.string()
     .min(5, "Title is too short")
