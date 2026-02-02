73245ecf773767ced78410c71b0533a86d664086
Sameer Kumar <sameerkumar10122004@gmail.com>
2026-02-02 16:07:17 +0530

    g

--- plugin/src/App.jsx
+++ plugin/src/App.jsx
@@ -54,7 +54,7 @@ const PluginApp = ({ userId, websiteId }) => {
         setFormVisible(false);
         resetForm();
       } catch (error) {
-        toast.error("Failed to submit bug report. ❌", { id: "submit-toast" },error);
+        toast.error("Failed to submit bug report. ❌", { id: "submit-toast" });
         console.error("Error submitting bug report:", error);
       } finally {
         setSubmitting(false);
